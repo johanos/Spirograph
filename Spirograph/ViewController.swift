c37d06dfa6558631633f0ff2fe8d2793a6b13d45
Johan Ospina <Johanos@bu.edu>
2016-06-25 20:18:43 -0400

    created project

---
+++ Spirograph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spirograph
+//
+//  Created by Johan Ospina on 6/25/16.
+//  Copyright © 2016 Johan Ospina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
